0c2796b1af571c28e555b1a7277bb1bf2225ea76
jizx <1822980003@qq.com>
2019-05-13 16:05:24 +0800

    将页面改了问题可下拉选择的形式，预测时通过 ajax 请求，目前还是没有加载模型的版本

--- web.py
+++ web.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, jsonify
 import json
 import numpy as np
 import codecs
@@ -10,106 +10,120 @@ app = Flask(__name__, template_folder='template/')
 from tqdm import tqdm
 import random
 import sys
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-random.seed(100)
+random.seed(99)
 
-qa_list = []
-with codecs.open("data/raw/dev-v1.1.json", 'r',encoding='utf-8') as f:
+############################### 加载预测结果、验证集文件 ###############################
+passage_list = []
+with codecs.open("data/raw/dev-v1.1.json", 'r', encoding='utf-8') as f:
     data = json.load(f)
     dev_data = data['data']
 
-with codecs.open("data/prediction.json", 'r',encoding='utf-8') as f:
+with codecs.open("data/prediction.json", 'r', encoding='utf-8') as f:
     predict_dict = json.load(f)
 
 for i in tqdm(range(len(dev_data)), desc="process... dev data"):
     paragraphs = dev_data[i]['paragraphs']
     for paragraph in paragraphs:
-        context = paragraph['context']
-        context = context.replace("''", '" ').replace("``", '" ').replace('　', ' ')
+        context = paragraph['context'].replace("''", '" ').replace("``", '" ').replace('　', ' ')
+
         qas = paragraph['qas']
+        question_answers = []
         for qa in qas:
-            question = qa['question']
             uuid = qa['id']
+            question = qa['question']
             answers = []
             for i, ans in enumerate(qa['answers']):
                 answers.append(qa['answers'][i]['text'])
 
-            entry = (uuid, context, question, " ✔️ ".join(answers))
-            qa_list.append(entry)
+            question_answers.append((uuid, question, " ✔️ ".join(answers)))
 
+        passage_list.append({"context": context, "qas": question_answers})
+
+
+############################### 辅助函数 ###############################
 
 def percent(prob_list):
+    ''' 将 softmax 得到的结果 在进行计算百分比，用于显示字体大小 '''
     min = np.min(prob_list)
-    probs_ = [prob - min for prob in prob_list]
-    sum = np.sum(probs_)
-    probs = [round(prob / sum, 4) for prob in probs_]
+    sum = np.sum(prob_list) - len(prob_list)*min
+    probs = [round(prob - min / sum, 4) for prob in prob_list]
     print(probs)
     print(max(probs))
     return probs
 
 
-def render_span(tokens, probs,color="9, 220, 123"):
+def render_span(tokens, probs, color="9, 220, 123"):
+    ''' 渲染显示概率的 html'''
     # 9, 132, 220 蓝
     # 9, 220, 123 青
     start_spans = ('<span style=" ' \
                    'background-color:rgba({},{:.2f}); ' \
                    'font-size:{}px;' \
-                   'border-radius:5px;">{}</span> '.format(color, 0.1 + start_prob, 13 + start_prob * 20, token) for
-                   token, start_prob in zip(tokens, probs))
+                   'border-radius:5px;">{}</span> '.format(color, 0.1 + prob, 13 + prob * 20, token) for
+                   token, prob in zip(tokens, probs))
     return ' '.join(start_spans)
 
+
+############################### API ###############################
 
 @app.route('/')
 @app.route('/<int:num>')
 def home(num=None):
-    # i = 1945 ,2759可以举例
+    # 1458 文章可以举例
+    # 1570
 
-    total_num = len(qa_list)
-    i = random.randint(0, total_num - 1) if not num or num >= total_num else num
-    entry = qa_list[i]
+    total_num = len(passage_list)  # 总数
+    context_index = random.randint(0, total_num - 1) if not num or num >= total_num else num  # 随机生成或者指定文章编号
+    passage = passage_list[context_index]
 
-    return render_template('home.html', index=i, context=entry[1], question=entry[2], answer=entry[3], predict="")
+    context = passage['context']
+    qas = passage['qas']
+    question_index = random.randint(0, len(qas) - 1)
+    uqa = qas[question_index]
+
+    data = {
+        'context_index': context_index,
+        'context': context,
+        'question_index': question_index,
+        'question': uqa[1],
+        'qas': qas,
+        'true_answer': uqa[2],
+        'predict_answer': ""
+    }
+    return render_template('home.html', **data)
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    index = str(request.form['index']).strip()
-    context = str(request.form['context']).strip()
-    question = str(request.form['question']).strip()
-    answer = str(request.form['answer']).strip()
-    entry = qa_list[int(index)]
-
-    # predict_entry = predict_dict.get(entry[0] + "\n", None)
-    predict_entry = predict_dict.get(entry[0], None)
-
-    # 跳过文章长度过长
+    predict_entry = predict_dict.get(request.form['uuid'], None)
     if predict_entry is None:
-        predict = "没有预测答案，可能原因：文章长度过长或者有特殊token"
-        return render_template('home.html', index=index, context=context, context_prob="", question=question, answer=answer,
-                               predict=predict)
+        result = {
+            'predict': "没有预测答案，可能原因：文章长度过长或者有特殊token"
+        }
+    else:
+        predict_answer = predict_entry['answer']
+        context_tokens = predict_entry['tokens']
+        context_len = len(context_tokens)
+        start_probs = predict_entry['start_probs'][:context_len]
+        end_probs = predict_entry['end_probs'][:context_len]
 
-    predict_answer = predict_entry['answer']
-    context_tokens = predict_entry['tokens']
-    context_len = len(context_tokens)
-    start_probs = predict_entry['start_probs'][:context_len]
-    end_probs = predict_entry['end_probs'][:context_len]
+        start_probs = percent(start_probs)
+        end_probs = percent(end_probs)
+        # 渲染 html
+        context_start_prob_html = render_span(context_tokens, start_probs)
+        context_end_prob_html = render_span(context_tokens, end_probs, color="9,132,220")
 
-    start_probs = percent(start_probs)
-    end_probs = percent(end_probs)
-
-    # 渲染 html
-    context_start_prob_html = render_span(context_tokens, start_probs,color="220,9,9")
-    context_end_prob_html = render_span(context_tokens, end_probs,color="9,132,220")
-
-    print("id:", index)
-    print(entry[0])
-    print(repr(predict_answer))
-
-    return render_template('home.html', index=index, context=context,
-                           context_start_prob=context_start_prob_html, context_end_prob=context_end_prob_html,
-                           question=question, answer=answer, predict=predict_answer)
+        result = {
+            'success': "true",
+            'context_start_prob': context_start_prob_html,
+            'context_end_prob': context_end_prob_html,
+            'predict_answer': predict_answer
+        }
+    return jsonify(result)
 
 
 if __name__ == '__main__':
