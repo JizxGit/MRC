6b5849b391acd226c460b1b73c3c016d26bdc182
jizx <1822980003@qq.com>
2019-05-18 20:54:57 +0800

    添加 log 功能

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from preprocess import main as prepro
 from model import Model
 from evaluate import print_test_score
 from config import config
+import logging as log
 
 
 def initial_model(session, ckpt_path, expect_exists=False):
-    print("Looking for model at {}...".format(ckpt_path))
+    log.info("Looking for model at {}...".format(ckpt_path))
     ckpt = tf.train.get_checkpoint_state(ckpt_path)
     v2_path = ckpt.model_checkpoint_path + ".index" if ckpt else ""
     if ckpt and (tf.gfile.Exists(ckpt.model_checkpoint_path) or tf.gfile.Exists(v2_path)):
-        print("Reading model parameters from {}".format(ckpt.model_checkpoint_path))
         saver = tf.train.Saver()
         saver.restore(session, ckpt.model_checkpoint_path)
     else:
         if expect_exists:
             raise Exception("There is no saved checkpoint at {}".format(ckpt_path))
         else:
-            print('initial model...')
+            log.info('initial model...')
             session.run(tf.global_variables_initializer())
 
 
@@ -40,6 +40,7 @@ def main(unused_argv):
     sess_config = tf.ConfigProto(allow_soft_placement=True)  # 是否打印设备分配日志;如果你指定的设备不存在,允许TF自动分配设备 
     sess_config.gpu_options.allow_growth = True  # 动态申请显存
 
+    log.info("### 『{}』 model is working in『 {}』 mode，batch_size：『{}』###".format(config.experiment_name, config.mode, config.batch_size))
     if config.mode == 'train':
         with tf.Session(config=sess_config) as sess:
             initial_model(sess, config.ckpt_path)
